307d0da707f85388a6725314fabe95540e3047f4
Silver_108 <parasnegi36@gmail.com>
2024-12-10 02:43:04 +0530

    Add image assets and update home page

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -12,29 +12,23 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color.fromRGBO(26, 30, 26, 1),
-      appBar: AppBar(
         backgroundColor: const Color.fromRGBO(26, 30, 26, 1),
-        leading: IconButton(
-            icon: const Icon(
-              Icons.menu,
-              color: Colors.white,
-              size: 30,
-            ),
-            onPressed: () {}),
-        title: Text(
-          "Pay Pulse",
-          style: GoogleFonts.comicNeue(
-              color: Colors.white, fontSize: 30, fontWeight: FontWeight.w600),
-        ),
-        actions: const [
-          Icon(
-            Icons.notifications_none_rounded,
-            size: 30,
-            color: Colors.white,
+        appBar: AppBar(
+          centerTitle: true,
+          backgroundColor: const Color.fromRGBO(26, 30, 26, 1),
+          title: Text(
+            "I Am Rich",
+            style: GoogleFonts.comicNeue(
+                color: Colors.white, fontSize: 30, fontWeight: FontWeight.w600),
           ),
-        ],
-      ),
-    );
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/diamond.png',
+            ),
+          ],
+        ));
   }
 }
